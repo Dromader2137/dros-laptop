df3a0fc48ac568e49b90b9a98002563b8e4bb422
Dromader2137 <wojciechowski.ignacy@proton.me>
2024-10-19 13:12:57 +0200

    laptop

---
+++ .mozilla/firefox/bssaeqkc.default-release/user.js
@@ -0,0 +1,14 @@
+user_pref("accessibility.typeaheadfind.flashBar", 0);
+user_pref("app.shield.optoutstudies.enabled", false);
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.bookmarks.addedImportButton", true);
+user_pref("browser.bookmarks.restore_default_bookmarks", false);
+user_pref("browser.contentblocking.category", "standard");
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("devtools.chrome.enabled", true);
+user_pref("devtools.debugger.remote-enabled", true);
+user_pref("devtools.everOpened", true);
+user_pref("extensions.formautofill.addresses.enabled", false);
+user_pref("extensions.formautofill.creditCards.enabled", false);
+user_pref("signon.rememberSignons", false);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
